24b0905c0ac5061262be68943ecd397d2dd0027f
Surdy-A <sodiq.ajayi@yahho.com>
2020-04-08 17:15:25 +0000

    Initial Commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'uadgb+py@-j4@8tdvb5^md((t*u_mqyy74%^hfto(&t*#(!s*b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ajayisodiq.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = '/home/ajayisodiq/MyPortfolio/static'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
